7250d767bc8ffb6e12263f629826b2c05af8600b
Jesus Ramirez <jrmz.maldonado@gmail.com>
2021-09-10 19:41:19 -0500

    Add Page component and GlobalStyles

---
+++ components/Page.js
@@ -0,0 +1,45 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  *,
+  *::after,
+  *::before {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    --regular: 'Sarabun-Regular';
+    --bold: 'Sarabun-Bold';
+    --white: #fff;
+    --primaryColor: #191b1f;
+    --secondaryColor: #ff8125;
+    --bgColor: #f3f3f3;
+  }
+
+  h1,
+  h2,
+  h3,
+  p,
+  span,
+  a {
+    margin: 0;
+    padding: 0;
+  }
+
+  button {
+    outline: none;
+    border: none;
+    background: none;
+  }
+`;
+
+export default function Page({ children }) {
+  return (
+    <div>
+      <GlobalStyles />
+      {children}
+    </div>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,15 @@ import { config, library } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import '../components/styles/styles.css';
+import Page from '../components/Page';
 
 config.autoAddCss = false;
 library.add(fab);
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Page>
+      <Component {...pageProps} />;
+    </Page>
+  );
 }
