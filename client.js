655807812e02df622c59f669625b6df422e4888b
Jesus Ramirez <jrmz.maldonado@gmail.com>
2021-09-15 18:04:34 -0500

    Add posts page with connection to Sanity CMS

--- client.js
+++ client.js
@@ -6,4 +6,5 @@ import sanityClient from '@sanity/client';
 export default sanityClient({
   projectId: 'nxsxkxc5',
   dataset: 'production',
+  useCdn: true, // `false` if you want to ensure fresh data
 });
---
+++ pages/posts/index.js
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import sanityClient from '../../client';
+
+function PostsPage({ allPosts }) {
+  return (
+    <main>
+      <section>
+        <h1>Blog Post Page</h1>
+        <h2>Welcome to my page of blog posts</h2>
+        <div>
+          {allPosts.map((post, index) => (
+            <article key={post.slug.current}>
+              <span>
+                <img
+                  src={post.mainImage.asset.url}
+                  alt={post.mainImage.alt}
+                ></img>
+                <span>
+                  <h3>{post.title}</h3>
+                </span>
+              </span>
+            </article>
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export async function getStaticProps() {
+  const allPosts = await sanityClient.fetch(
+    `*[_type == "post"]{
+                  title,
+                  slug,
+                  mainImage {
+                      asset->{
+                          _id,
+                          url
+                      },
+                      alt
+                  }
+              }`
+  );
+  return {
+    props: {
+      allPosts,
+    },
+  };
+}
+
+export default PostsPage;
