6c911763e632e8f78ecf97294c4f136e88226b5e
Jesus Ramirez <jrmz.maldonado@gmail.com>
2021-09-17 14:27:30 -0500

    Add experiment schema

---
+++ studio/schemas/experiment.js
@@ -0,0 +1,30 @@
+export default {
+  name: 'experiment',
+  title: 'Experiment',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'date',
+      title: 'Date',
+      type: 'datetime',
+    },
+    {
+      name: 'link',
+      title: 'Live Link',
+      type: 'url',
+    },
+    {
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
