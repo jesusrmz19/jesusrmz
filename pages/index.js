618104559d24e82219b669fe5319040e2ce52444
Jesus Ramirez <jrmz.maldonado@gmail.com>
2021-09-29 18:05:22 -0500

    Add SEO for the home page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import Button from '../components/styles/Button';
 import Link from 'next/link';
+import Head from 'next/head';
 import FirstFold from '../components/styles/FirstFold';
 import LatestWork from '../components/styles/LatestWork';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -43,79 +44,113 @@ function Home({ allProjects, allExperiments, allBlogPosts, allSideProjects }) {
     closeNav();
   }, []);
   return (
-    <main>
-      <FirstFold>
-        <div className="container">
-          <h1>¡Hola!</h1>
-          <p>
-            My name is Jesús, and I'm a self-taught web developer that uses{' '}
-            <span>React</span>,<span> JavaScript</span>, and plain{' '}
-            <span>CSS</span> &<span> HTML</span> to build cool things.
-          </p>
-          <div className="links">
-            <a href="https://github.com/jesusrmz19" className="links--icons">
-              <FontAwesomeIcon icon={['fab', 'github']} />
-            </a>
-            <a href="https://codepen.io/jesusrmz" className="links--icons">
-              <FontAwesomeIcon icon={['fab', 'codepen']} />
-            </a>
-            <a href="https://twitter.com/jesusrmz_" className="links--icons">
-              <FontAwesomeIcon icon={['fab', 'twitter']} />
-            </a>
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Hola! I'm Jesus, and I'm a freelance web developer. I help businesses increase competitiveness by designing and developing great-looking and functional websites."
+        />
+        <meta name="twitter:title" content="Jesus Ramirez - Web Developer" />
+        <meta
+          name="twitter:description"
+          content="Hola! I'm Jesus, and I'm a freelance web developer. I help businesses increase competitiveness by designing and developing great-looking and functional websites."
+        />
+        <meta name="twitter:image" content="" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content="https://jesusrmz.com/" />
+        <meta property="og:title" content="Jesus Ramirez- Web Developer" />
+        <meta
+          property="og:description"
+          content="Hola! I'm Jesus, and I'm a freelance web developer. I help businesses increase competitiveness by designing and developing great-looking and functional websites."
+        />
+        <meta property="og:image" content="" />
+        <meta property="og:url" content="https://jesusrmz.com/" />
+        <meta
+          name="description"
+          content="Hola! I'm Jesus, and I'm a freelance web developer. I help businesses increase competitiveness by designing and developing great-looking and functional websites."
+        />
+        {/* <link rel="icon" href="/img/logo_negro.png" /> */}
+        <title>Jesus Ramirez - Web Developer</title>
+      </Head>
+      <main>
+        <FirstFold>
+          <div className="container">
+            <h1>¡Hola!</h1>
+            <p>
+              My name is Jesús, and I'm a self-taught web developer that uses{' '}
+              <span>React</span>,<span> JavaScript</span>, and plain{' '}
+              <span>CSS</span> &<span> HTML</span> to build cool things.
+            </p>
+            <div className="links">
+              <a href="https://github.com/jesusrmz19" className="links--icons">
+                <FontAwesomeIcon icon={['fab', 'github']} />
+              </a>
+              <a href="https://codepen.io/jesusrmz" className="links--icons">
+                <FontAwesomeIcon icon={['fab', 'codepen']} />
+              </a>
+              <a href="https://twitter.com/jesusrmz_" className="links--icons">
+                <FontAwesomeIcon icon={['fab', 'twitter']} />
+              </a>
+            </div>
+            <div className="container--btn">
+              <a href="#work">EXPLORE WORK</a>
+            </div>
           </div>
-          <div className="container--btn">
-            <a href="#work">EXPLORE WORK</a>
+        </FirstFold>
+        <LatestWork id="work">
+          <h3>Latest Work</h3>
+          <div className="work">
+            {allProjects.map((project) => (
+              <Project key={project.title} details={project} />
+            ))}
           </div>
-        </div>
-      </FirstFold>
-      <LatestWork id="work">
-        <h3>Latest Work</h3>
-        <div className="work">
-          {allProjects.map((project) => (
-            <Project key={project.title} details={project} />
-          ))}
-        </div>
-      </LatestWork>
-      <ExperimentsSection>
-        <h3>Experiments</h3>
-        <div className="experiments">
-          {allExperiments.map((experiment) => (
-            <Experiment
-              key={experiment.title}
-              details={experiment}
-            ></Experiment>
-          ))}
-        </div>
-      </ExperimentsSection>
-      <LatestWork>
-        <h3>Last Side Projects</h3>
-        <div className="work">
-          <Project details={allSideProjects[0]}></Project>
-        </div>
-      </LatestWork>
-      <RecentBlogs>
-        <h3>Recent Blogs</h3>
-        <div className="blogs">
-          {allBlogPosts.map((post) => (
-            <Link href={`/posts/${post.slug.current}`} key={post.slug.current}>
-              <Card className="card">
-                <div className="card--img">
-                  <img
-                    src={post.mainImage.asset.url}
-                    alt={post.mainImage.alt}
-                  ></img>
-                </div>
-                <div className="card--txt">
-                  <h4 className="card--title">{post.title}</h4>
-                  <p className="card--description">{post.description}</p>
-                  <p className="card--date">{setDate(post.date)}</p>
-                </div>
-              </Card>
-            </Link>
-          ))}
-        </div>
-      </RecentBlogs>
-    </main>
+        </LatestWork>
+        <ExperimentsSection>
+          <h3>Experiments</h3>
+          <div className="experiments">
+            {allExperiments.map((experiment) => (
+              <Experiment
+                key={experiment.title}
+                details={experiment}
+              ></Experiment>
+            ))}
+          </div>
+        </ExperimentsSection>
+        <LatestWork>
+          <h3>Last Side Projects</h3>
+          <div className="work">
+            <Project details={allSideProjects[0]}></Project>
+          </div>
+        </LatestWork>
+        <RecentBlogs>
+          <h3>Recent Blogs</h3>
+          <div className="blogs">
+            {allBlogPosts.map((post) => (
+              <Link
+                href={`/posts/${post.slug.current}`}
+                key={post.slug.current}
+              >
+                <Card className="card">
+                  <div className="card--img">
+                    <img
+                      src={post.mainImage.asset.url}
+                      alt={post.mainImage.alt}
+                    ></img>
+                  </div>
+                  <div className="card--txt">
+                    <h4 className="card--title">{post.title}</h4>
+                    <p className="card--description">{post.description}</p>
+                    <p className="card--date">{setDate(post.date)}</p>
+                  </div>
+                </Card>
+              </Link>
+            ))}
+          </div>
+        </RecentBlogs>
+      </main>
+    </>
   );
 }
 
